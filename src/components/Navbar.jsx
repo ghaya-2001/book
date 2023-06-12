ff2e8a768167869651774f51623074ec68f4afa6
ghayaderbali <ghayaderbali2001@gmail.com>
2023-06-12 15:40:16 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.div`
+  display: flex;
+  justify-content: center;
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0px;
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    padding: 10px;
+  }
+`;
+
+const Links = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 50px;
+`;
+
+const Logo = styled.img`
+  height: 70px;
+  padding-top:10%;
+`;
+
+const List = styled.ul`
+  display: flex;
+  gap: 20px;
+  list-style: none;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const ListItem = styled.li`
+  cursor: pointer;
+`;
+
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+const Icon = styled.img`
+  width: 20px;
+  cursor: pointer;
+`;
+
+const Button = styled.button`
+  width: 100px;
+  padding: 10px;
+  background-color: #03C1AD;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Navbar = () => {
+  return (
+    <Section>
+      <Container>
+        <Links>
+          <Logo src="./img/studenty.png" />
+         
+        </Links>
+        <Icons>
+          {/* Changed the image due to copyright problems */}
+          <Icon src="./img/search.png" />
+          <Button>Get to know</Button>
+        </Icons>
+      </Container>
+    </Section>
+  );
+};
+
+export default Navbar;
